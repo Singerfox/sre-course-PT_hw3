8d3eabbf13ab59e1d0815367e8d10c76353bbcc2
Singerfox <147327237+Singerfox@users.noreply.github.com>
2023-11-26 18:34:10 +0300

    Add files via upload

---
+++ kill-test.js
@@ -0,0 +1,32 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+  discardResponseBodies: true,
+  thresholds: {
+    http_req_failed: [{ threshold: 'rate<0.03', abortOnFail: true }],
+    http_req_duration: [{ threshold: 'p(90)<1000', abortOnFail: true }],
+    checks: ['rate>0.9'],
+  },
+  scenarios: {
+   breakpoint_test: {
+      executor: 'ramping-arrival-rate',
+          preAllocatedVUs: 350,
+    stages: [
+     { duration: '15m', target: 3000 },
+    ],
+  },
+ },
+};
+
+export default function reads() {
+  const url = `http://sre-course.singerfox.ru/WeatherForecast`;
+  const params = {
+    headers: {
+      'accept':'application/json',
+    },
+  };
+  check(http.get(url, params), {
+    'status 200': (r) => r.status === 200,
+  });
+}
---
+++ load-test.js
@@ -0,0 +1,36 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+  thresholds: {
+    http_req_failed: [{ threshold: 'rate<0.01', abortOnFail: true }],
+    http_req_duration: [{ threshold: 'p(90)<500', abortOnFail: true }],
+    checks: ['rate>0.9'],
+  },
+  scenarios: {
+    reads: {
+      executor: 'ramping-arrival-rate',
+      preAllocatedVUs: 75,
+      exec: 'read',
+    stages: [
+     { duration: '1m', target: 5000 },
+     { duration: '3m', target: 5000 },
+     { duration: '1m', target: 0 },
+    ],
+   },
+  },
+};
+
+export function read() {
+  const url = `http://sre-course.singerfox.ru/cities/5`;
+  const params = {
+    headers: {
+      'accept':'application/json',
+    },
+  };
+  check(http.get(url, params), {
+    'status 200': (r) => r.status === 200,
+     'correct api response': (r) =>
+      r.body.includes('Kostroma'),
+  });
+}
